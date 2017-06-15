6e3523d7d5b3189e4d50cbaf0e1a93e388ed865d
lingkdong <lingkdong@outlook.com>
2017-06-15 11:40:08 +0800

    add test

---
+++ src/test/java/unit/AbstractTest.java
@@ -0,0 +1,13 @@
+package unit;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by DT254 on 2017/6/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-test/spring*.xml"})
+public class AbstractTest {
+}
---
+++ src/test/java/unit/UserServiceTest.java
@@ -0,0 +1,16 @@
+package unit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by DT254 on 2017/6/15.
+ */
+public class UserServiceTest extends AbstractTest {
+
+    @Test
+    public void testFindUserByName() throws Exception {
+        System.out.println(1);
+    }
+}
